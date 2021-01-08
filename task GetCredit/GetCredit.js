eb40f6aa1301e43159c608184b2c815fdf00bdbe
AnnaSovpel <anna.sovpel97@mail.ru>
2021-01-08 13:29:43 +0300

    Create task_GetCredit

--- task GetCredit/GetCredit.js
+++ task GetCredit/GetCredit.js
@@ -3,33 +3,30 @@
 function getCredid_if_you_are_18 () {
     let age = prompt('How old are you?',18)
     
-    
-    if (age == +age & +age >= 18) {
+    if (!isNaN(age) && +age >= 18) {
       getCredfitSum()
-    }
-    
-    else if (age == +age & +age < 18) {
+    } else if (!isNaN(age) && +age < 18) {
       alert ('you can\'t leave a request');
-    }
-    
-    else if (age === age) {
+    } else if (isNaN(age)) {
       alert ('Please try again and enter a number');
+      getCredid_if_you_are_18();
     }
     
-    }
+}
     
+function getCredfitSum () {
+    let sum = prompt('Enter the amount',500)
     
-    function getCredfitSum (){
-    let sum = +prompt('Enter the amount',500)
-    
-    if (sum === + sum & sum <= 1000){
+    if (!isNaN(sum) && sum <= 1000 && sum !== ''){
         document.write(`Your application for ${sum } $ will be accepted`);
-    }
-    else if (sum === + sum & sum > 1000){
+    } else if (!isNaN(sum) && sum > 1000){
         alert('Maximum loan amount - 1000$')
-    }
-    }
+    } else if (isNaN(sum)){
+      alert ('Please try again and enter a number witout spaces');
+      getCredfitSum ();
+    } 
+}
     
   
-    getCredid_if_you_are_18();
+    // getCredid_if_you_are_18();
 
