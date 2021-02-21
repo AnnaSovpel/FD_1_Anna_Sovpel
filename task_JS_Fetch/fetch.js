ee32d47d7243de669f6dcb76f4648077ee417f32
AnnaSovpel <anna.sovpel97@mail.ru>
2021-02-21 12:38:40 +0300

    JS_task_xhr_fetch

---
+++ task_JS_Fetch/fetch.js
@@ -0,0 +1,22 @@
+const xhr = new XMLHttpRequest();
+xhr.open ('GET', 'https://jsonplaceholder.typicode.com/users')
+xhr.onload = function (){
+    let answer = xhr.response;
+    console.log(answer);
+}
+xhr.onerror = function(){
+    console.log(error)
+}
+xhr.send()
+
+
+fetch ('https://jsonplaceholder.typicode.com/users')
+.then (response => {
+    let a = response.json()
+    const p = new Promise(resolve => resolve(a))
+    p.then (user => {
+        for (i = 0; i<user.length; i++){
+            console.log(user[i].name)}
+    })
+})
+.catch (error => console.log(error))
