be891180a3952dda6e0f41541fab9222b2531abf
AnnaSovpel <anna.sovpel97@mail.ru>
2021-02-24 12:39:46 +0300

    Create task (use async/await)

--- task_JS_Fetch/fetch.js
+++ task_JS_Fetch/fetch.js
@@ -10,13 +10,22 @@ xhr.onerror = function(){
 xhr.send()
 
 
-fetch ('https://jsonplaceholder.typicode.com/users')
-.then (response => {
-    let a = response.json()
-    const p = new Promise(resolve => resolve(a))
-    p.then (user => {
-        for (i = 0; i<user.length; i++){
-            console.log(user[i].name)}
-    })
-})
-.catch (error => console.log(error))
+// fetch ('https://jsonplaceholder.typicode.com/users')
+// .then (response => {
+//     let a = response.json()
+//     const p = new Promise(resolve => resolve(a))
+//     p.then (user => {
+//         for (i = 0; i<user.length; i++){
+//             console.log(user[i].name)}
+//     })
+// })
+// .catch (error => console.log(error))
+
+async function showUserName() {
+
+    let response = await fetch('https://jsonplaceholder.typicode.com/users');
+    let user = await response.json();
+    for (i = 0; i<user.length; i++){
+        console.log(user[i].name)}
+  }
+  showUserName();
